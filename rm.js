68bbba31a106ee9e3711d54c580e2e240d374fb8
Dinesh Padmanaban <dpadmanaban@walmart.com>
2016-01-05 13:32:44 -0800

    rm

--- rm.js
+++ rm.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env babel-node
 
 "use strict";
 
@@ -6,38 +6,28 @@ let fs = require('pn/fs')
 let path = require('path')
 let _ = require('lodash')
 
-if (process.argv.length < 3) {
-        console.error('Usage: ls.js <file name> <options>');
-        process.exit(1);
-}
-
 var rootPath = process.argv[2]
-var param
-var recursive = false
 
-
-
-function rm(rootPath)
+ async function rm(rootPath)
 {
      try 
      {
         let lspromises = []
-        fs.stat(rootPath).then(function (data){
+        let data = await fs.stat(rootPath)
         if(data.isFile()){
            console.log("removing file"+rootPath)
-            fs.unlink(rootPath).then(console.log("removing file"+rootPath))
+            let data = await fs.unlink(rootPath)
+            console.log("removing file"+rootPath)
         }
         else{
-         fs.readdir(rootPath).then(function(fileNames){
+        	 let fileNames =await fs.readdir(rootPath)
              for (let fileName of fileNames) 
              {
                 let filePath = path.join(rootPath, fileName)
-                let promise = rm(filePath)
-
-          
+                await rm(filePath)
             }
               fs.rmdir(rootPath).then(console.log("removing directory "+rootPath))
-        })}})
+        }
     } catch (e) {
         console.log(e.stack)
     }
